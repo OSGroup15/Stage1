f4be3c259298b7afb33b6bef7f7a39116fc3f094
zmace <zmace@users.noreply.github.com>
2015-03-24 15:43:49 -0400

    Update BinaryBuddy.java
    allocate() should be all done now.  It looks to be working good.  Someone will have to do deallocate() and fix searchProcess() to make deallocate() work right.

--- BinaryBuddy.java
+++ BinaryBuddy.java
@@ -9,55 +9,67 @@ package tree;
  *
  *
  */
+import java.util.ArrayList;
+
 public class BinaryBuddy {
 
     private static Tree tree;
     private static Tree.TreeNode root;
-    private static int div;
-
+    private static int div;    //the result of TreeNode.memSize / requested mem
+    private static int memoryUsed;  //sum of memory space used
+    private static ArrayList<Tree.TreeNode> freeList = new ArrayList<Tree.TreeNode>();
+                     // holds a free memory blocks ready to hold a process
     /**
      * Constructor for BinaryBuddy
      */
     public BinaryBuddy() {
         tree = new Tree();
         root = tree.new TreeNode(64, "free");
+        memoryUsed = 0;
+        freeList.add(root);
     }
 
+    /**
+     * a method that recursively allocates a memory block, if available
+     * @param size int, requested memory size
+     * @param proc String, name of process- A, B, C, etc
+     * @return a TreeNode
+     */
     public static Tree.TreeNode allocate(int size, String proc) {
+        if (memoryUsed + size > 64) {
+            System.out.println("no more space available for process " + proc);
+            return null;
+        }
         Tree.TreeNode newNode;
         newNode = searchSpace(size, root);
-        if (newNode != null && newNode.process.equals("free") && tree.isLeaf(newNode)) {
+        if (newNode != null && div == 1) {
             newNode.process = proc;
+            memoryUsed = newNode.memSize + memoryUsed;
             System.out.println("Space for process " + proc + " found");
             return newNode;
         }
-        
-        if (newNode == null && div > 1) {
-            double newSize;
-            int power;
-            power = tree.treeHeightDriver(root);
-
-            newSize = Math.pow(2, 6 - power);
-            int i = (int) newSize;
-            tree.insertNode(i, "free", root);
+        if ((newNode != null && div > 1)) {
+            int i = newNode.memSize / 2;
+            tree.insertNode(i, "free", newNode);
             return allocate(size, proc);
         }
-        if (newNode == null && (div == 1 || div == 0)) {
-            System.out.println("no space available");
+        if (newNode == null && (div == 0 || div == 1)) {
+            System.out.println("no space available for process " + proc);
         }
-        
-//        else{
-//            System.out.println("No space for process " + proc);
-//        }
 
         return null;
     }
 
+    /**
+     * TO DO
+     * @param proc 
+     */
     public static void deallocate(String proc) {
 
     }
 
     /**
+     * MIGHT NEED TO BE REDONE
      * a method that traverses the nodes of the tree to find a specified name of
      * a process -- A,B,C, etc.
      *
@@ -81,58 +93,65 @@ public class BinaryBuddy {
         return null;
     }
 
+    /**
+     * a method that traverses the nodes of the tree to find a node with the
+     * proper space available
+     *
+     * @param mem int, requested size of process
+     * @param node TreeNode,
+     * @return the node where mem can be stored
+     */
     public static Tree.TreeNode searchSpace(int mem, Tree.TreeNode node) {
-        if (node != null) {
-            div = node.memSize / mem;
-            if (div ==0){
-                return null;
-            }
-            if (div == 1 && node.process.equals("free")) {
-                return node;
-            }
-
-            if ((node.left) != null) {
-                return searchSpace(mem, node.left);
-            }
-            if ((node.parent.right) != null) {
-                return searchSpace(mem, node.parent.right);
+        Tree.TreeNode t = null;
+        freeList.clear();
+        getFreeNodes(root);
+        for (int i = 0; i < freeList.size(); i++) {
+            t = freeList.get(i);
+            div = t.memSize / mem;
+            if (div == 1) {
+                freeList.clear();
+                return t;
             }
         }
-        //System.out.println("no space available");
-        return null;
+        if (div == 0) {
+            freeList.clear();
+            return null;
+        }
+        freeList.clear();
+        return t;
     }
-//  THis method used for testing
-//    public static Tree.TreeNode searchSpace(int mem, Tree.TreeNode node) {
-//        if (node != null) {
-//            if ((node.memSize / mem) == 1) {
-//                return node;
-//            }
-//
-//            if ((node.left) != null) {
-//                return searchSpace(mem, node.left);
-//            }
-//            if ((node.right) != null) {
-//                return searchSpace(mem, node.right);
-//            }
-//        }
-//        System.out.println("no space available");
-//        return null;
-//    }
+
+
+   
+    /**
+     * a method that fills ArrayList of TreeNodes with free space
+     * @param t a TreeNode
+     */
+    public static void getFreeNodes(Tree.TreeNode t) {
+        if (t == null) {
+            return;
+        }
+        if (t.left == null && t.right == null && t.process.equals("free")) {
+            freeList.add(t);
+        }
+        getFreeNodes(t.left);
+        getFreeNodes(t.right);
+    }
 
     public static void main(String args[]) {
         new BinaryBuddy();
 
-//        tree.insertNode(32, "free", root);
-//        tree.insertNode(16, "free", root);
-//        tree.insertNode(8, "free", root);
-//        tree.insertNode(4, "free", root);
-//        System.out.println(tree.treeHeightDriver(root));
-        allocate(32, "A");
-        allocate(32, "B");
-        //allocate(13, "C");
-        //Tree.TreeNode n = searchSpace(4, root);
-        //System.out.println(div);
-        //System.out.println(n.toString(n));
+        allocate(23, "A");
+        allocate(3, "B");
+        allocate(4, "C");
+        allocate(17, "D");
+        allocate(2, "E");
+
+        
+        System.out.println();
+        System.out.println("Free and Used Memory Bloacks:");
+        tree.printLeafNodes(root);
+
     }
 
 }
